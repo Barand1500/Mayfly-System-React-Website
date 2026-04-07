0ffdf2f82bcfaf928fab06affc873a84e82c4b1c
Barand1500 <116197447+Barand1500@users.noreply.github.com>
2026-04-07 17:43:07 +0300

    Ana sayfa routing düzeltmesi - Mayfly-System-React-Website URL'i ile

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/baranuruncan">
+    <BrowserRouter basename="/Mayfly-System-React-Website">
       <App />
     </BrowserRouter>
   </StrictMode>,
