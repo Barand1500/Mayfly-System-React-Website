9400b897e70d17041b0940fef02752134a148c0d
Barand1500 <116197447+Barand1500@users.noreply.github.com>
2026-04-07 17:37:04 +0300

    Doğru GitHub Pages URL'si ile güncellendi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MayflySystems-React/',
+  base: '/Mayfly-System-React-Website/',
   build: {
     minify: false,
     cssMinify: false,
